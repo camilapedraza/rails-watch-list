85eb227f4b9baa5890c7ec25924d04146c68affc
Camila Pedraza <36695574+camilapedraza@users.noreply.github.com>
2023-05-23 22:08:18 +0200

    CSS updates with Le Wagon Style

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,10 @@
 Rails.application.routes.draw do
-  get 'bookmarks/new'
-  get 'bookmarks/create'
-  get 'bookmarks/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-  resources :lists, only: %i[index show new create] do
+  root to: 'lists#index'
+  resources :lists, only: %i[show new create] do
     resources :bookmarks, only: %i[new create]
   end
   # delete 'bookmarks/:id', to: 'bookmarks#destroy'
